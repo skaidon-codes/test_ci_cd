f58a208d7f0f3ab17796116e7fa5891641f55235
skaidon-codes <102329314+skaidon-codes@users.noreply.github.com>
2025-06-16 20:09:44 -0400

    Create math.py

---
+++ math.py
@@ -0,0 +1,11 @@
+def multiply(numbers):
+    product = 1
+    for n in numbers:
+	      product = product * n
+    return 1
+
+def add(numbers):
+    sum = 0
+    for n in numbers:
+	      sum = sum + n
+    return sum
